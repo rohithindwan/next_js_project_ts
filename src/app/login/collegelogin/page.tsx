44c9b3787ec3c7c61bdf6b57e535d668fcf890af
Rohit Hindwan <rohithindwan123@gmail.com>
2024-02-28 23:50:04 +0530

    use nested navigator

---
+++ src/app/login/collegelogin/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+import Link from 'next/link'
+// import styles from "../page.module.css";
+
+export default function CollegeLogin() {
+  function handleform (e :any){
+    
+    const form = e.target
+    let formData = new FormData(form)
+    const formObj =Object.fromEntries(formData.entries())
+    console.log(formObj)
+  }
+
+  return (
+    <main className="check">
+
+      <div className="login-container">
+        <h2>college login </h2>
+        <form onSubmit={handleform} className="login-form">
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <div className="form-group">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
