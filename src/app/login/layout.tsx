5f792f00f1ac89d0cd393c14ecdac6c94201f0eb
Rohit Hindwan <rohithindwan123@gmail.com>
2024-02-29 23:51:25 +0530

    add nav bar using next layout and use conditional layout

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,14 +1,33 @@
+'use client'
+
+import Link from 'next/link'
+import './login.css'
+import { usePathname } from "next/navigation"
 export default function LoginLayout({
-    children, // will be a page or nested layout
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <section>
-        {/* Include shared UI here e.g. a header or sidebar */}
-        <nav></nav>
-   
-        {children}
-      </section>
-    )
-  }
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode
+}) {
+  const router = usePathname()
+  console.log(router)
+
+  return (
+    <section>
+      {
+        router !== "/login/collegelogin" ?
+        <ul className='login-menu'>
+          <li> <Link href="/">Home</Link></li>
+          <li> <Link href="/login">Login Page</Link></li>
+          <li> <Link href="/login/studentlogin">Student Login</Link></li>
+          <li> <Link href="/login/collegelogin">College Login</Link></li>
+        </ul> :
+        <ul className='login-menu'>
+          <li> <Link href="/login">Login Page</Link></li>
+        </ul>
+      }
+
+
+      {children}
+    </section>
+  )
+}
